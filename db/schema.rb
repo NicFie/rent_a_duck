9f96b1b66f8f55d0f9e71a0835a15fb1fd4512a2
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-29 20:03:01 +0200

    duck and booking models added, index action for ducks implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_143801) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_152533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "duck_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["duck_id"], name: "index_bookings_on_duck_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "ducks", force: :cascade do |t|
     t.string "name"
@@ -35,5 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_143801) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookings", "ducks"
+  add_foreign_key "bookings", "users"
   add_foreign_key "ducks", "users"
 end
