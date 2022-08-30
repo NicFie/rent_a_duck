f49de008f62ae4e7b5c9d4608b5c61b0e42bc517
Nicola Field <nicola.field2@gmail.com>
2022-08-30 14:50:20 +0100

    Removing status presence true validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :duck
 
-  validates :status, presence: true
+
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
